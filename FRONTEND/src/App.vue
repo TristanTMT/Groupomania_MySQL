7cf04e2a100307fdc7175ff5e222c690ffd4433d
TristanTMT <57182040+TristanTMT@users.noreply.github.com>
2021-04-06 07:13:48 +0400

    Add files via upload

---
+++ FRONTEND/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+html{
+  box-sizing: border-box;
+}
+
+body {
+    margin:0;
+    padding:0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
+  color: inherit;
+}
+</style>
---
+++ FRONTEND/src/views/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="signup">
+    <SignupForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignupForm from '@/components/SignupForm.vue';
+
+export default {
+  name: 'Signup',
+
+  components: {
+    SignupForm
+  },
+}
+</script>
+
+<style lang="scss">
+body{
+  background: url("../../public/assets/fond.jpg") no-repeat center fixed; 
+  -webkit-background-size: cover; /* pour anciens Chrome et Safari */
+  background-size: cover; /* version standardisée */
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  background: white;
+  padding: 0 1rem 1rem 1rem;
+  max-width: 500px;
+  background: rgba(30, 130, 76, .5);
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px 2px rgba(0,0,0,0.68);
+}
+</style>
